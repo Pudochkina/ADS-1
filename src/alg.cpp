c4b2a0fe87f1a4cf45eb1baa452ce7a1e7d04b28
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-02-17 22:39:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,19 +5,15 @@
 
 bool checkPrime(uint64_t value) {
 int count = 0;
-for (int i = 2; i <= 100000; i++)
-{
-if (value % i == 0)
-{
+for (int i = 2; i <= 100000; i++){
+if (value % i == 0){
 count++;
 }
 }
-if (count == 1)
-{
+if (count == 1){
 return true;
 }
-else
-{
+else{
 return false;
 }
 }
@@ -25,8 +21,7 @@ return false;
 uint64_t nPrime(uint64_t n) { 
 int value = 11;
 int count = 0;
-switch (n)
-{
+switch (n){
 case (1):
 value = 2;
 return value;
@@ -44,15 +39,12 @@ value = 7;
 return value;
 break;
 }
-if (n > 4)
-{
-for (int i = 11; i <= 100000; i++)
-{
-if ((value % i == 0) && (value % 2 == 1) && (value % 5 != 0) && (value % 3 != 0) && (value % 7 != 0))
-{
+if (n > 4){
+for (int i = 11; i <= 100000; i++){
+if ((value % i == 0) && (value % 2 == 1)
+    && (value % 5 != 0) && (value % 3 != 0) && (value % 7 != 0)){
 count++;
-if (count == n)
-{
+if (count == n){
 return value;
 }
 }
@@ -63,8 +55,7 @@ value++;
 
 uint64_t nextPrime(uint64_t value) {
 int count3 = 0;
-switch (value)
-{
+switch (value){
 case (2):
 value = 3;
 return value;
@@ -82,15 +73,13 @@ value = 11;
 return value;
 break;
 }
-if (value >= 11)
-{
-for (int i = value; i <= 100000; i++)
-{
-if ((value % i == 0) && (value % 2 == 1) && (value % 5 != 0) && (value % 3 != 0) && (value % 7 != 0))
+if (value >= 11){
+for (int i = value; i <= 100000; i++){
+if ((value % i == 0) && (value % 2 == 1) 
+    && (value % 5 != 0) && (value % 3 != 0) && (value % 7 != 0))
 {
 count3++;
-if (count3 == 2)
-{
+if (count3 == 2){
 return value;
 }
 }
@@ -102,34 +91,27 @@ value++;
 uint64_t sumPrime(uint64_t hbound) {
 int result = 0;
 int value3 = 11;
-if (hbound <= 11 && hbound >= 7)
-{
+if (hbound <= 11 && hbound >= 7){
 result = 17;
 return result;
 }
-else if (hbound >= 5 && hbound <= 7)
-{
+else if (hbound >= 5 && hbound <= 7){
 result = 10;
 return result;
 }
-else if (hbound >= 3 && hbound <= 5)
-{
+else if (hbound >= 3 && hbound <= 5){
 result = 5;
 return result;
 }
-else if (hbound <= 3)
-{
+else if (hbound <= 3){
 result = 2;
 return result;
 }
 
-if (hbound > 11)
-{
+if (hbound > 11){
 result = 17;
-for (int i = value3; i < hbound; i++)
-{
-if ((value3 % i == 0) && (value3 % 2 == 1) && (value3 % 5 != 0) && (value3 % 3 != 0) && (value3 % 7 != 0))
-{
+for (int i = value3; i < hbound; i++){
+if ((value3 % i == 0) && (value3 % 2 == 1) && (value3 % 5 != 0) && (value3 % 3 != 0) && (value3 % 7 != 0)){
 result += value3;
 }
 value3++;
