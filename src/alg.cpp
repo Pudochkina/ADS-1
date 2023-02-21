5283ea618ddf8175a2a38e5443368449cb5d361f
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-02-21 23:19:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,6 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <math.h>
 #include "alg.h"
 
 
@@ -18,91 +19,71 @@ return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-int value2 = 11;
-int count2 = 0;
-int result2 = 0;
-switch (n) {
-case (1):
-result2 = 2;
-break;
-case (2):
-result2 = 3;
-break;
-case (3):
-result2 = 5;
-break;
-case (4):
-result2 = 7;
-break;
-}
-if (n > 4) {
-for (int i = 11; i <= 10000000; i++) {
-if ((value2 % i == 0) && (value2 % 2 == 1)
-&& (value2 % 5 != 0) && (value2 % 3 != 0) && (value2 % 7 != 0)) {
-count2++;
-if (count2 == (n-4)) {
-result2 = value2;
+int result2 = 0;  
+int count = 0;
+int porNum = 0;
+for (int value = 2; value <= 10000000; value++) {
+int provNum = sqrt(value);
+for (int i = provNum; i > 1; i--) {
+if (value % i != 0) {
+count++;
 }
 }
-value2++;
+if (count == provNum - 1) {
+porNum++;
+}
+count = 0;
+if (n == porNum) {
+result2 = value;
+break;
 }
 }
 return result2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-int count3 = 0;
+int ishNumber = 0;
 int result3 = 0;
-switch (value) {
-case (2):
-result3 = 3;
-break;
-case (3):
-result3 = 5;
-break;
-case (5):
-result3 = 7;
-break;
-case (7):
-result3 = 11;
-break;
-}
-if (value >= 11) {
-for (int i = value; i <= 10000000; i++) {
-if ((value % i == 0) && (value % 2 == 1)
-&& (value % 5 != 0) && (value % 3 != 0) && (value % 7 != 0)) {
-count3++;
-if (count3 == 2) {
-result3 = value;
+int count = 0;
+int porNum = 0;
+for (int value3 = 2; value3 <= 1000000; value3++) {
+int provNum = sqrt(value3);
+for (int i = provNum; i > 1; i--) {
+if (value3 % i != 0) {
+count++;
 }
 }
-value++;
+if (count == provNum - 1) {
+porNum++;
+}
+count = 0;
+if (value3 == value) {
+ishNumber = porNum;
+}
+if ((value3 > value) && (porNum == ishNumber + 1)) {
+result3 = value3;
+break;
 }
 }
 return result3;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-int sum = 0;
-int value4 = 11;
-if (hbound <= 11 && hbound >= 7) {
-sum = 17;
-} else if (hbound >= 5 && hbound <= 7) {
-sum = 10;
-} else if (hbound >= 3 && hbound <= 5) {
-sum = 5;
-} else if (hbound <= 3)  {
-sum = 2;
+int count = 0;
+int porNum = 0;
+long long sum = 0;
+for (long long value = 2; value < hbound; value++) {
+int provNum = sqrt(value);
+for (int i = provNum; i > 1; i--) {
+if (value % i != 0) {
+count++;
 }
-if (hbound > 11) {
-sum = 17;
-for (int i = value4; i < hbound; i++) {
-if ((value4 % i == 0) && (value4 % 2 == 1)
-&& (value4 % 5 != 0) && (value4 % 3 != 0) && (value4 % 7 != 0)) {
-sum += value4;
 }
-value4++;
+if (count == provNum - 1) {
+porNum++;
+sum += value;
 }
+count = 0;
 }
 return sum;
 }
